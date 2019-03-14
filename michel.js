9081bd3c42e2199e255327faac262fa604fabfa2
Frédéric NOËL <contact@fredericnoel.com>
2019-03-14 10:14:42 +0100

    INstanciation objet Etudiant(). Passage par référence.

--- michel.js
+++ michel.js
@@ -1,10 +1,14 @@
-var etudiant = {
-    nom : "Auchecorne",
-    prenom : "Michel",
-    age : 47,
-    getInfo : function() {
-        return "Je m'appelle " + this.prenom;
-    }
-};
+function Etudiant(first, name, age) {
+    this.prenom = first;
+    this.nom = name;
+    this.age = age;
+}
 
-alert(etudiant.getInfo());
+var etudiant1 = new Etudiant("Auchecorne", "Michel", 47);
+
+var etudiant2 = etudiant1;
+
+etudiant2.prenom = "Gerard";
+
+alert(etudiant2.prenom);
+alert(etudiant1.prenom);
